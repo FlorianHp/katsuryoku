d50c8e5d3e996da459c05e242bf0c0bb4a91b738
FlorianHp <layho@outlook.de>
2025-01-22 21:48:52 +0100

    style: responsive

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const modal = document.getElementById('modal');
+const modal      = document.getElementById('modal');
+const menu       = document.getElementById('breadcrumbs');
+const burgerMenu = document.getElementById('menu');
 
 const openModal = () => {
   modal.showModal();
@@ -7,3 +9,39 @@ const openModal = () => {
 const closeModal = () => {
   modal.close();
 };
+
+const handleClickOutside = (e) => {
+
+  if (!menu.contains(e.target) && e.target !== burgerMenu) {
+    menu.classList.remove('open');
+    window.removeEventListener('click', handleClickOutside);
+  }
+};
+
+const openMenu = () => {
+  menu.classList.add('open');
+  console.log('OPEN GESETZT🐒')
+  setTimeout(() => {
+    window.addEventListener('click', handleClickOutside)
+  },500 );
+};
+
+const closeMenu = () => {
+  menu.classList.remove('open');
+  console.log('OPEN ENTFERNT')
+  window.removeEventListener('click', handleClickOutside);
+  
+};
+
+const toggleMenu = (e) => {
+ /*  e.stopPropagation(); */
+  if (menu.classList.contains('open')) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+};
+
+if (burgerMenu) { 
+  burgerMenu.addEventListener('click', toggleMenu);
+}
